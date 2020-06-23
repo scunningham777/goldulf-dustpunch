54d9b64c38fc4527e6763a752f15a3a3f63b6606
Sean <s.cunningham777@gmail.com>
2020-06-23 16:52:21 -0500

    Start procdural maps

--- src/scenes/dungeon.ts
+++ src/scenes/dungeon.ts
@@ -2,15 +2,25 @@ import Hero from '../objects/hero';
 import { WORLD_WIDTH, WORLD_HEIGHT, GAME_SCALE } from '../constants';
 import { Cardinal_Direction } from '../utils';
 
+const MAP_KEY = 'dungeon-map';
+const LAYER_KEYS = {
+    BG_LAYER: 'bg-layer',
+    STUFF_LAYER: 'stuff-layer',
+    DUST_LAYER: 'dust-layer',
+};
+
 export class DungeonScene extends Phaser.Scene {
 
     private hero: Hero;
     private cursors: any;
     private dungeon: Phaser.Physics.Arcade.Image;
+    private map: Phaser.Tilemaps.Tilemap;
+    private mapLayers = new Map<string, Phaser.Tilemaps.DynamicTilemapLayer>();
+    private tileset: Phaser.Tilemaps.Tileset;
 
     create(): void {
-        this.hero = new Hero(100, WORLD_HEIGHT / 2, this, 160, Cardinal_Direction.RIGHT);
-        this.addDungeons();
+        this.createMap();
+        this.createHero();
         this.addCollisions();
         this.cursors = this.input.keyboard.createCursorKeys();
     }
@@ -19,6 +29,29 @@ export class DungeonScene extends Phaser.Scene {
         if (!!this.hero) {
             this.hero.update(this.cursors);
         }
+    }
+    
+    createMap() {
+        this.map = this.make.tilemap({
+            tileWidth: 32,
+            tileHeight: 32,
+            width: 30,
+            height: 30,
+            key: MAP_KEY,
+        });
+        this.tileset = this.map.addTilesetImage('terrain', 'terrain', 32, 32);
+        for (const keyIndex in LAYER_KEYS) {
+            this.mapLayers.set(LAYER_KEYS[keyIndex], this.map.createBlankDynamicLayer(LAYER_KEYS[keyIndex], this.tileset));
+        }
+        
+        this.mapLayers.get(LAYER_KEYS.BG_LAYER).fill(30);
+        this.mapLayers.get(LAYER_KEYS.BG_LAYER).setScale(GAME_SCALE);
+        
+        this.addDungeons();
+    }
+
+    createHero() {
+        this.hero = new Hero(100, WORLD_HEIGHT / 2, this, 360, Cardinal_Direction.RIGHT);
     }
 
     addDungeons() {
--- src/scenes/overworld.ts
+++ src/scenes/overworld.ts
@@ -1,23 +1,55 @@
 import Hero from '../objects/hero';
 import { WORLD_WIDTH, WORLD_HEIGHT, GAME_SCALE } from '../constants';
 
+const MAP_KEY = 'overworld-map';
+const LAYER_KEYS = {
+    BG_LAYER: 'bg-layer',
+    STUFF_LAYER: 'stuff-layer',
+};
+
 export class OverworldScene extends Phaser.Scene {
 
     private hero: Hero;
     private cursors: any;
     private dungeon: Phaser.Physics.Arcade.Image;
+    private map: Phaser.Tilemaps.Tilemap;
+    private mapLayers = new Map<string, Phaser.Tilemaps.DynamicTilemapLayer>();
+    private tileset: Phaser.Tilemaps.Tileset;
 
     create(): void {
-        this.hero = new Hero(WORLD_WIDTH / 2, 100, this, 160);
-        this.addDungeons();
+        this.createMap();
+        this.createHero();
         this.addCollisions();
         this.cursors = this.input.keyboard.createCursorKeys();
     }
-
+    
     update(): void {
         if (!!this.hero) {
             this.hero.update(this.cursors);
         }
+    }
+    
+    createMap() {
+        this.map = this.make.tilemap({
+            tileWidth: 32,
+            tileHeight: 32,
+            width: 30,
+            height: 30,
+            key: MAP_KEY,
+        });
+        this.tileset = this.map.addTilesetImage('terrain', 'terrain', 32, 32);
+        for (const keyIndex in LAYER_KEYS) {
+            this.mapLayers.set(LAYER_KEYS[keyIndex], this.map.createBlankDynamicLayer(LAYER_KEYS[keyIndex], this.tileset));
+        }
+        
+        this.mapLayers.get(LAYER_KEYS.BG_LAYER).fill(5);
+        this.mapLayers.get(LAYER_KEYS.BG_LAYER).setScale(GAME_SCALE);
+
+        this.addDungeons();
+    }
+    
+    createHero() {
+        this.hero = new Hero(WORLD_WIDTH / 2, 100, this, 260);
     }
 
     addDungeons() {
