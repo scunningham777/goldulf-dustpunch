34c1179c3ebf767d250aa9b61a3fff6babe8a898
Sean <s.cunningham777@gmail.com>
2020-07-03 23:14:24 -0500

    Initial firebase deploy

--- src/game.ts
+++ src/game.ts
@@ -22,7 +22,7 @@ const config: Phaser.Types.Core.GameConfig = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
+            debug: false,
             gravity: {
                 y: 0,
             },
